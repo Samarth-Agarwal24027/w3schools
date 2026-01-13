7a5a9b461e9b3d0eada1fbb3b85b6fecb986cacd
Samarth-Agarwal24027 <24027@burnside.school.nz>
2026-01-13 15:26:09 +1300

    first test

--- mypython.py
+++ mypython.py
@@ -1,3 +1,8 @@
-cars = ["BMW", "Audi", "Toyota"]
+cars = ["Tata Motors", "Mahindra", "Maruti Suzuki", "Hyundai", "Kia", "Toyota"]
 for i in cars:
-    print("I like python")
+    print(i)
+    if i == "Hyundai":
+        break
+        print("This is inside the loop")
+    else:
+        print("This is outside the loop")
